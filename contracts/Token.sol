f6cfda2d2fe4d658017fb34ad66c36dc32d402b4
Jack Piro <jackpiro1133@gmail.com>
2023-01-31 02:30:55 -0800

    2. Smoke Test

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    //creaing the public view type is going to give us access to a method that allows us to read the varible name.
+    string public name = "My Token";
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhoast: {url: "http://127.0.0.1:8545"}
+  },
 };
